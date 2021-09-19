1d7cbbae15584676f32abe8d2b332a801c33cc20
Sampriti-Mitra <sampriti.mitra@razorpay.com>
2021-09-19 21:44:44 +0530

    added token file with empty tokens

---
+++ config/token.go
@@ -0,0 +1,12 @@
+package config
+
+const (
+	AWS_ACCESS_KEY        = ""
+	AWS_SECRET_ACCESS_KEY = ""
+	AWS_REGION            = ""
+	AWS_BUCKET            = ""
+
+	APPLICATION_DB_USERNAME = ""
+	APPLICATION_DB_PASSWORD = ""
+	APPLICATION_DB_NAME     = ""
+)
